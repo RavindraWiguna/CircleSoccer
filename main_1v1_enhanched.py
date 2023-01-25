110a220bfc9cbb7ce9e1b1e1cf7d1a0a2f38dd94
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2023-01-25 19:21:35 +0700

    gedein fitness ballz

--- main_1v1_enhanched.py
+++ main_1v1_enhanched.py
@@ -401,7 +401,7 @@ def calculate_ball_fitness(player, ball):
     final_distance_ball = calculate_distance(player.body.position, ball.body.position)
     max_fitness = calculate_distance((0,0), (WIDTH, HEIGHT))
     fitness = 1 - final_distance_ball/max_fitness
-    fitness *=100
+    fitness *=1000
     return fitness
 
 ### ==== MAIN FUNCTION ==== ###
